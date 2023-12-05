acf0a8bf2df79beda1a0b06a3fff197bbe25ce4c
Shuo Huang <128951219+hs31415@users.noreply.github.com>
2023-12-05 15:01:42 +0800

    文件结构完善
    + 初步尝试能否将不同功能放到不同文件中

---
+++ library-backend/register.py
@@ -0,0 +1,33 @@
+import pymysql
+import bcrypt
+from fastapi import HTTPException
+from pydantic import BaseModel
+
+class UserRegistration(BaseModel):
+    account: str
+    password: str
+
+async def login(user_data: UserRegistration, conn: pymysql.connections.Connection):
+    try:
+        account = user_data.account
+        password = user_data.password
+        # 创建游标对象
+        with conn.cursor() as cursor:
+            # 查询账号是否已存在
+            sql = "SELECT COUNT(*) FROM readers WHERE account = %s"
+            cursor.execute(sql, (account,))
+            result = cursor.fetchone()
+            if result[0] > 0:
+                return {'message': '账号已存在'}
+
+            # 使用bcrypt进行密码加密
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+            print(hashed_password)
+            # 执行插入操作
+            sql = "INSERT INTO readers (account, name, password) VALUES (%s, %s, %s)"
+            cursor.execute(sql, (account, f"用户{account}", hashed_password))
+            # 提交事务
+            conn.commit()
+        return {'message': '注册成功'}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
